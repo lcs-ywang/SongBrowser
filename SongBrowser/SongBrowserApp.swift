0bbaa5dcf494a5deba89264325fe2f5e676a3290
yining wang <ywang@lcs.on.ca>
2021-02-20 09:49:35 -0500

    Initial Commit

---
+++ SongBrowser/SongBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  SongBrowserApp.swift
+//  SongBrowser
+//
+//  Created by Yining Wang on 2021-02-20.
+//
+
+import SwiftUI
+
+@main
+struct SongBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
