b15f974525ae485be49ee3e7540ce3a34f55cbb8
yining wang <ywang@lcs.on.ca>
2021-02-20 09:59:14 -0500

    We got the Search bar to appear at the top of the view

--- SongBrowser/ContentView.swift
+++ SongBrowser/ContentView.swift
@@ -8,9 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //MARK: Stored properties
+    
+    //kkep trach of what the user search for
+    
+    @State private var searchText = ""
+    
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack{
+            SearchBarView(text: $searchText)
+            
+            //Push the search bar to the top
+            
+            Spacer()
+            
+        }
     }
 }
 
